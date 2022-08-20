71b99dfd8a648a2afc8550cfa102efff65dacd11
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-20 18:55:31 -0300

    :sparkles: feat: criar novos usuarios / POST

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -5,6 +5,9 @@ export default function Layout() {
 
     const [buscaapi, setBuscaapi] = useState()
 
+    const [namevalue, setNamevalue] = useState()
+    const [emailvalue, setEmailvalue] = useState()
+
     // useEffect(() => {
     //     axios.get("https://viacep.com.br/ws/29934738/json/")
     //         .then((response) => setBuscaapi(response.data))
@@ -21,11 +24,32 @@ export default function Layout() {
         .then((response) => setBuscaapi(response.data))
     }, [])
 
+    const submit = () => {
+        axios.post("http://localhost:3001/users/create", {  
+            name: namevalue,
+            email: emailvalue
+        })
+    }
+
+    const getName = (event) => setNamevalue(event.target.value)
+    const getEmail = (event) => setEmailvalue(event.target.value)
+
     return (
         <div>
+            <div>
+                <form action="">
+                    <p>Adicione as informações para criar um usuario</p>
+                    <input type="text" name="name" placeholder="nome" required onChange={getName}/>
+                    <input type="text" name="email" placeholder="email" required onChange={getEmail}/>
+                    <button type="submit" onClick={() => submit()}>Criar Usuario</button>
+                </form>
+            </div>
             {buscaapi?.map(printresposta => (
                 <li key={printresposta.id}>
-                    <div>{printresposta.name}</div>
+                    <div className="infos-box-content">
+                        <div className="infos-content name">{printresposta.name}</div>
+                        <div className="infos-content email">{printresposta.email}</div>
+                    </div>
                 </li>
             ))}
         </div> // API PEGANDO INFO PERCORRENDO ENTRE OBJETOS
