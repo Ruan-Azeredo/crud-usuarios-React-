126ff5da4e3000d5c134d7451bdbb990642cb15c
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-24 17:37:41 -0300

    :recycle: refactor: cria local de funções HTTP

---
+++ src/functions/create.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+
+export default function create(namevalue, emailvalue){
+    axios.post("http://localhost:3001/users/create", {  
+        name: namevalue,
+        email: emailvalue
+    })
+}
