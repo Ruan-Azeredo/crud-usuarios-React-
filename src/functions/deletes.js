009f7021cc7f8cc1fdfe13b410a0a7d334cd74c1
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-24 19:07:55 -0300

    :bulb: docs: comentario função deletes

--- src/functions/deletes.js
+++ src/functions/deletes.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-export default function deletes(data) {
+export default function deletes(data) { // A função 'delete()' já existe
     const id = data.id
 
     axios.delete(`http://localhost:3001/users/${id}`)
