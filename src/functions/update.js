69304b53274d34c1b65b1694196117a49863819e
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-24 17:36:19 -0300

    :sparkles: feat: adiciona Update

---
+++ src/functions/update.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+export default function update(data, namevalue, emailvalue) {
+
+    const id = data.id
+    console.log({namevalue, emailvalue, id})
+
+    axios.put(`http://localhost:3001/users/${id}`, {
+        name: namevalue,
+        email: emailvalue
+    })
+    document.location.reload()
+}
