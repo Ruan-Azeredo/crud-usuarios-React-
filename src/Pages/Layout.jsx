ce050acf495c2403b360a78f90d077f8ec702f55
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-25 19:13:06 -0300

    :lipstick: feat: adiciona estilização

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import IndexRow from "../Components/IndexRow";
 import ModalTemplate from "../Components/ModalTemplate";
 const axios = require('axios');
 
@@ -12,21 +13,31 @@ export default function Layout() {
     }, [])
 
     return (
-        <div>
-            <div>
-            <ModalTemplate acao='create' />
-            </div>
-            {buscaapi?.map(printresposta => (
-                <li key={printresposta.id}>
-                    <div className="infos-box-content">
-                        <div className="infos-content name">{printresposta.name}</div>
-                        <div className="infos-content email">{printresposta.email}</div>
+        <div className={
+            `bg-gradient-to-r from-mainColor to-secondColor pt-[2%]`
+        }>
+            <div className={`text-mainColor bg-white-100 w-[10%] py-[1%]  justify-center font-IriaSans flex text-3xl`}>USERS</div>
+            <div className={`bg-white-100 px-[5%] min-h-screen h-full`}>
+                <div className={`pb-[1%] pt-[3%]`}>
+                    <ModalTemplate acao='create' />
+                </div>
+                {buscaapi?.map(printresposta => (
+                    <div key={printresposta.id}
+                        className={`
+                            bg-white-50 mt-6 flex justify-between py-6 px-12
+                        `}>
+                        <IndexRow printresposta={printresposta} />
+                        <div>
+                            <ModalTemplate printresposta={printresposta} acao='info' />
+                            <ModalTemplate printresposta={printresposta} acao='update' />
+                            <ModalTemplate printresposta={printresposta} acao='delete' />
+                        </div>
                     </div>
-                    <ModalTemplate printresposta={printresposta} acao='info' />
-                    <ModalTemplate printresposta={printresposta} acao='update' />
-                    <ModalTemplate printresposta={printresposta} acao='delete' />
-                </li>
-            ))}
+                ))}
+            </div>
+            <div className="p-6 left-20 flex bg-white-100">
+                    <span className={`text-textColor text-xl`}>Feito com &#9829; por </span><span className={`text-mainColor font-bold text-xl`}>Ruan Azeredo</span>
+                </div>
         </div>
     )
 }
