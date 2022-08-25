d33bb9fee5f17ffa279372c81ce14ef13dc61931
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-25 10:55:33 -0300

    :package: build: adiciona tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Pages/**/*.{js,ts,jsx,tsx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx}",
+    "./src/Components/TagComponents/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
