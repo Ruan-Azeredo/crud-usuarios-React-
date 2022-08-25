4433d6b49cdc5e649a5322595be934a9a56df6d5
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-25 19:11:52 -0300

    :fire: chore: retira console.log()

--- src/functions/update.js
+++ src/functions/update.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 export default function update(data, namevalue, emailvalue) {
 
     const id = data.id
-    console.log({namevalue, emailvalue, id})
 
     axios.put(`http://localhost:3001/users/${id}`, {
         name: namevalue,
