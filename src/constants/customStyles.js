aea1bc467d5c36950d4d217b586b193022c8d062
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-25 23:52:02 -0300

    :sparkles: feat: separa customStyles

---
+++ src/constants/customStyles.js
@@ -0,0 +1,13 @@
+export const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: '#EBE2F1',
+      width: '1000px',
+      padding: '0'
+    },
+};
