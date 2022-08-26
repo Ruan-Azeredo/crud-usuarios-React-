b265121f866ad4754d6521f6ca91d1b5035e1896
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-08-25 23:50:55 -0300

    :recycle: refactor: adiciona novos components

--- src/Components/ModalTemplate.jsx
+++ src/Components/ModalTemplate.jsx
@@ -1,4 +1,3 @@
-import Modal from 'react-modal'
 import React, { useState } from "react"
 import update from '../functions/update';
 import create from "../functions/create";
@@ -12,6 +11,8 @@ import FooterModalComponent from './TagComponents/FooterModalComponent';
 import ModalButtonComponent from './TagComponents/ModalButtonComponent';
 import TitleModalComponent from './TagComponents/TitleModalComponent';
 import AvisoDeleteComponent from './TagComponents/AvisoDeleteComponent';
+import ContentButtonsComponent from "./TagComponents/ContentButtonsComponent";
+import { customStyles } from "../constants/customStyles";
 
 export default function ModalTemplate(params) {
 
@@ -20,21 +21,6 @@ export default function ModalTemplate(params) {
 
     const [namevalue, setNamevalue] = useState()
     const [emailvalue, setEmailvalue] = useState()
-
-    const customStyles = {
-        content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-          backgroundColor: '#EBE2F1',
-          width: '1000px',
-          padding: '0'
-        },
-    };
-
 
     const openModal = () => {
         setIsOpen(true);
@@ -64,12 +50,15 @@ export default function ModalTemplate(params) {
                 onClick={acao != 'create' ? () => getArray(params) : () => openModal()}
                 value={acao.toUpperCase()}
             ></ButtonComponent>
+
             <ModalComponent isOpen={modalIsOpen} onRequestClose={closeModal} data={data} style={customStyles}>
-                <TitleModalComponent value={acao.toUpperCase()} hidden={acao != 'delete' ? null : 'hidden'}/>
-                <div className={`
-                        flex flex-row
-                        `}>
+
+                <TitleModalComponent value={acao.toUpperCase()} hidden={acao != 'delete' ? null : 'hidden'} />
+                
+                <ContentButtonsComponent>
+
                     <div>
+
                         <LabelComponent hidden={acao == 'delete' ? true : false} title={'NAME'}>
                         <InputComponent
                                 defaultValue={acao == 'update' || 'info' ? data.name : null}
@@ -78,6 +67,7 @@ export default function ModalTemplate(params) {
                                 hidden={acao == 'delete' ? true : false}
                             />
                         </LabelComponent>
+
                         <LabelComponent hidden={acao == 'delete' ? true : false} title={'EMAIL'}>
                             <InputComponent
                                 defaultValue={acao == 'update' || 'info' ? data.email : null}
@@ -86,14 +76,17 @@ export default function ModalTemplate(params) {
                                 hidden={acao == 'delete' ? true : false}
                             />
                         </LabelComponent>
+
                     </div>
-                    <IdComponent
-                        value={data.id}
-                        show={data.id && acao != 'delete' ? null : 'hidden'}/>
+
+                    <IdComponent value={data.id} show={data.id && acao != 'delete' ? null : 'hidden'}/>
                     
-                </div>
-                <AvisoDeleteComponent hidden={acao == 'delete' ? null : 'hidden'}/>
+                </ContentButtonsComponent>
+
+                <AvisoDeleteComponent hidden={acao == 'delete' ? null : 'hidden'} />
+                
                 <FooterModalComponent>
+
                     <ModalButtonComponent
                         onClick={() => create(namevalue, emailvalue)}
                         hidden={acao == 'create' ? false : true}
@@ -110,7 +103,9 @@ export default function ModalTemplate(params) {
                     >DELETE</ModalButtonComponent>
 
                     <ModalButtonComponent onClick={closeModal}>CLOSE</ModalButtonComponent>
+
                 </FooterModalComponent>
+
             </ModalComponent>
         </>
     )
